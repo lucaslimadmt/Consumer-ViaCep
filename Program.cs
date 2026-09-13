08716c20866d5ec052465ad552dec5dcb979be72
lucaslimadmt <lucaslimadmetrio@gmail.com>
2026-09-13 10:53:54 -0300

    Projeto comoleto Consumer ViaCep

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+WriteLine("Digite o seu CEP: ");
+var cep = ReadLine();
+
+var enderecoUrl = $@"https://viacep.com.br/ws/{cep}/json/";
+
+WriteLine($"Realizando requisição para o endpoint: {enderecoUrl}");
+
+var client = new HttpClient();
+
+try
+{
+    HttpResponseMessage? response = await client.GetAsync(enderecoUrl);
+    response.EnsureSuccessStatusCode();
+
+    string respostaApi = await response.Content.ReadAsStringAsync();
+
+    WriteLine(respostaApi);
+}
+catch (System.Exception e)
+{
+    WriteLine("Aconteceu um erro ao consultar a api: " + e.Message);
+}
